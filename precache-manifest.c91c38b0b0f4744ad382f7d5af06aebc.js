b5d05663954eb126ea83447ea87c0486ba8fecb4
addalant <addaepee@gmail.com>
2022-04-24 16:57:56 -0600

    Updates

---
+++ precache-manifest.c91c38b0b0f4744ad382f7d5af06aebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6017636674309e4d1d",
+    "url": "/suprablog.github.io/static/css/main.2a1f048f.chunk.css"
+  },
+  {
+    "revision": "7b6017636674309e4d1d",
+    "url": "/suprablog.github.io/static/js/main.161ca1e9.chunk.js"
+  },
+  {
+    "revision": "d33846bdca31d4aa75e4",
+    "url": "/suprablog.github.io/static/js/runtime~main.ea8a0fe1.js"
+  },
+  {
+    "revision": "8d3e4d7811e46e17f8eb",
+    "url": "/suprablog.github.io/static/js/2.54901cc3.chunk.js"
+  },
+  {
+    "revision": "ea463fa1a71f2e466552",
+    "url": "/suprablog.github.io/static/js/3.286fa367.chunk.js"
+  },
+  {
+    "revision": "3f0a667f83df7eff838324f07e141abd",
+    "url": "/suprablog.github.io/index.html"
+  }
+];
